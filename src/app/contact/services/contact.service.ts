43774c0e2ef55ef993e248d699fb2003d31f067f
Hanninenjp <jphanninen@gmail.com>
2018-05-16 13:11:26 +0300

    Added simple REST API interface implementation, added ContactHttpService, getting contacts from a server is supported.

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import {Contact} from '../contact';
+import {Observable} from 'rxjs';
+import {ContactHttpService} from './contact-http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  contacts: Contact[];
+  // contacts: Contact[];
 
-  constructor() {
+  constructor(private contactHttpService: ContactHttpService) {
+    /*
     this.contacts = [];
     this.contacts.push(new Contact(1, 'Sami', 'Anttonen'));
     this.contacts.push(new Contact(2, 'Joku', 'Toinen'));
     this.contacts.push(new Contact(3, 'Vielä', 'Kolmas'));
+    */
   }
 
+  /*
   getContacts(): Contact[] {
     return this.contacts;
+  }
+  */
+
+  getContacts(): Observable<Contact[]> {
+    return this.contactHttpService.get();
   }
 
 }
