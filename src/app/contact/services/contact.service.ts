4cebaa3f5bed98b3b105f4648a3c949517c2e5fc
Hanninenjp <jphanninen@gmail.com>
2018-05-17 11:10:50 +0300

    Added contact detail component, extended contact service and contact http service with functionality for getting single contact information from api interface, added simple error handling for getting single contact information from api interface.

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -15,4 +15,8 @@ export class ContactService {
     return this.contactHttpService.get();
   }
 
+  getContactById(id): Observable<Contact> {
+    return this.contactHttpService.getById(id);
+  }
+
 }
