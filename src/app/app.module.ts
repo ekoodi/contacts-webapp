53965631a38730f55eb2da3ac544a8489702f36a
santtone <sami.anttonen@trimble.com>
2018-05-21 12:38:37 +0300

    Added TextToColorPipe and ordered contact list by name.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import {ContactListComponent} from './contact/contact-list/contact-list.component';
 import {ContactService} from './contact/services/contact.service';
 import {ContactHttpService} from './contact/services/contact-http.service';
 import {HttpClientModule} from '@angular/common/http';
-import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import {ContactDetailComponent} from './contact/contact-detail/contact-detail.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MaterialComponentsModule} from './ui/material-components/material-components.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import { AvatarModule } from 'ng2-avatar';
+import {AvatarModule} from 'ng2-avatar';
+import {TextToColorPipe} from './contact/pipes/text-to-color.pipe';
+import {NgPipesModule} from 'ngx-pipes';
 
 const appRoutes: Routes = [
-  { path: 'contacts', component: ContactListComponent},
-  { path: 'contacts/:id', component: ContactDetailComponent},
-  { path: '', redirectTo: '/contacts', pathMatch: 'full'}
+  {path: 'contacts', component: ContactListComponent},
+  {path: 'contacts/new', component: ContactDetailComponent},
+  {path: 'contacts/:id', component: ContactDetailComponent},
+  {path: '', redirectTo: '/contacts', pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    TextToColorPipe
   ],
   imports: [
     BrowserModule,
@@ -32,8 +36,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     MaterialComponentsModule,
     FlexLayoutModule,
-    AvatarModule.forRoot()
-
+    AvatarModule.forRoot(),
+    NgPipesModule
   ],
   providers: [
     ContactService,
@@ -41,4 +45,5 @@ const appRoutes: Routes = [
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
